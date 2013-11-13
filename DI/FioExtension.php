7636c06845e7400661409944baca6af7f1e3c5f7
Milan Matejcek <milan.matejcek@gmail.com>
2013-11-13 10:47:11 +0100

    Minimum support

--- DI/FioExtension.php
+++ DI/FioExtension.php
@@ -6,9 +6,14 @@ use Nette\Configurator;
 use Nette\DI\CompilerExtension;
 use Nette\DI\Compiler;
 
-if (!defined('\Nette\Framework::VERSION_ID') || \Nette\Framework::VERSION_ID < 20100) {
-    class_alias('Nette\Config\CompilerExtension', 'Nette\DI\CompilerExtension');
-    class_alias('Nette\Config\Compiler', 'Nette\DI\Compiler');
+if (defined('\Nette\Framework::VERSION_ID') || Framework::VERSION_ID < 20100) {
+    if (!class_exists('Nette\DI\CompilerExtension')) {
+        class_alias('Nette\Config\CompilerExtension', 'Nette\DI\CompilerExtension');
+    }
+
+    if (!class_exists('Nette\DI\Compiler')) {
+        class_alias('Nette\Config\Compiler', 'Nette\DI\Compiler');
+    }
 }
 
 class FioExtension extends CompilerExtension {
