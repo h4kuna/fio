5a7ed11140ec7871a7ea2986746422c0a7e4e528
Milan Matějček <milan.matejcek@gmail.com>
2020-10-30 10:47:55 +0100

    Guzzle: update

--- tests/data/ClientMock.php
+++ tests/data/ClientMock.php
@@ -5,47 +5,96 @@ namespace h4kuna\Fio\Test;
 use GuzzleHttp\ClientInterface;
 use GuzzleHttp\Exception\ClientException;
 use GuzzleHttp\Exception\ServerException;
+use GuzzleHttp\Promise\PromiseInterface;
 use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
 
-class ClientMock implements ClientInterface
-{
-
-	public function send(RequestInterface $request, array $options = [])
+$classReflection = new \ReflectionClass(ClientInterface::class);
+$x = $classReflection->getMethod('requestAsync')->getParameters()[0]->getType();
+if ($x !== null && $x->getName() === 'string') {
+	class ClientMock implements ClientInterface
 	{
-		throw new \RuntimeException('Not implemented. ' . __METHOD__);
-	}
 
-
-	public function sendAsync(RequestInterface $request, array $options = [])
-	{
-		throw new \RuntimeException('Not implemented. ' . __METHOD__);
-	}
-
-
-	public function request($method, $uri, array $options = [])
-	{
-		$response = new Response($method, $uri, $options);
-		if (isset($options[Response::EXCEPTION_CLASS])) {
-			if (in_array($options[Response::EXCEPTION_CLASS], [ClientException::class, ServerException::class])) {
-				throw new $options[Response::EXCEPTION_CLASS]($uri, new Request(), $response);
-			} else {
-				throw new \RuntimeException('Unknown class.');
-			}
+		public function send(RequestInterface $request, array $options = []): ResponseInterface
+		{
+			throw new \RuntimeException('Not implemented. ' . __METHOD__);
 		}
 
-		return $response;
+
+		public function sendAsync(RequestInterface $request, array $options = []): PromiseInterface
+		{
+			throw new \RuntimeException('Not implemented. ' . __METHOD__);
+		}
+
+
+		public function request(string $method, $uri, array $options = []): ResponseInterface
+		{
+			$response = new Response($method, $uri, $options);
+			if (isset($options[Response::EXCEPTION_CLASS])) {
+				if (in_array($options[Response::EXCEPTION_CLASS], [ClientException::class, ServerException::class])) {
+					throw new $options[Response::EXCEPTION_CLASS]($uri, new Request(), $response);
+				} else {
+					throw new \RuntimeException('Unknown class.');
+				}
+			}
+
+			return $response;
+		}
+
+
+		public function requestAsync(string $method, $uri, array $options = []): PromiseInterface
+		{
+			throw new \RuntimeException('Not implemented. ' . __METHOD__);
+		}
+
+
+		public function getConfig(?string $option = null): ResponseInterface
+		{
+			throw new \RuntimeException('Not implemented. ' . __METHOD__);
+		}
+
 	}
-
-
-	public function requestAsync($method, $uri, array $options = [])
+} else {
+	class ClientMock implements ClientInterface
 	{
-		throw new \RuntimeException('Not implemented. ' . __METHOD__);
+
+		public function send(RequestInterface $request, array $options = [])
+		{
+			throw new \RuntimeException('Not implemented. ' . __METHOD__);
+		}
+
+
+		public function sendAsync(RequestInterface $request, array $options = [])
+		{
+			throw new \RuntimeException('Not implemented. ' . __METHOD__);
+		}
+
+
+		public function request($method, $uri, array $options = [])
+		{
+			$response = new Response($method, $uri, $options);
+			if (isset($options[Response::EXCEPTION_CLASS])) {
+				if (in_array($options[Response::EXCEPTION_CLASS], [ClientException::class, ServerException::class])) {
+					throw new $options[Response::EXCEPTION_CLASS]($uri, new Request(), $response);
+				} else {
+					throw new \RuntimeException('Unknown class.');
+				}
+			}
+
+			return $response;
+		}
+
+
+		public function requestAsync($method, $uri, array $options = [])
+		{
+			throw new \RuntimeException('Not implemented. ' . __METHOD__);
+		}
+
+
+		public function getConfig($option = null)
+		{
+			throw new \RuntimeException('Not implemented. ' . __METHOD__);
+		}
+
 	}
-
-
-	public function getConfig($option = null)
-	{
-		throw new \RuntimeException('Not implemented. ' . __METHOD__);
-	}
-
 }
